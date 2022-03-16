f02b926105f9b2173412e8e52499397c584dcf0f
Mikko Kiander <mikko@statzon.com>
2022-03-17 00:00:42 +0200

    Added removal/ingoring of non-utf-8 characters

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 import csv
 import sys
 
+def strip_non_ascii(string):
+    ''' Returns the string without non ASCII characters'''
+    stripped = (c for c in string if 0 < ord(c) < 127)
+    return ''.join(stripped)
+
 fileName = input("Filename:")
 rowsPerFile = input("Rows per output file:")
 
@@ -11,7 +16,7 @@ headerRow = []
 
 sys.stdout.write('\nSplitting file rows...\n')
 
-with open(fileName) as file:
+with open(fileName, encoding='utf-8', errors='ignore') as file:
     rows = csv.reader(file)
     for row in rows:
         if (rowCounter == -1 and fileCounter == 0):
@@ -33,8 +38,8 @@ sys.stdout.write('\nWriting new files...\n')
 
 for file in range(0, len(newFiles)):
     splitFileName = fileName + '_' + str(file) + '.csv'
-    with open(splitFileName, 'w+') as fout:
-        writer = csv.writer(fout)
+    with open(splitFileName, 'w+') as f:
+        writer = csv.writer(f)
         writer.writerows(newFiles[file])
 
 sys.stdout.write('\nDone!\n')
